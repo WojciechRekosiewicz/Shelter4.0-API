55e7b7a17e8a30441865e8037f14aa8ea6442b3e
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-07-03 19:16:05 +0200

    trying to get userscontroller to work

--- Shelter.API/Controllers/UsersController.cs
+++ Shelter.API/Controllers/UsersController.cs
@@ -7,36 +7,50 @@ using Microsoft.AspNetCore.Mvc;
 using Shelter.API.Contracts.Responses;
 using Shelter.API.Data.Repositories;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Shelter.API.Controllers
 {
 
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController : Controller
+    public class UsersController : Controller
     {
         private readonly IUserRepository _userRepository;
         private readonly UserManager<IdentityUser> _userManager;
 
-        public UserController(IUserRepository userRepository, UserManager<IdentityUser> userManager)
+        public UsersController(IUserRepository userRepository, UserManager<IdentityUser> userManager)
         {
             _userRepository = userRepository;
             _userManager = userManager;
         }
+        
 
         [HttpGet]
         public IActionResult List()
         {
             var users = _userRepository.GetAllUsers();
+            Console.WriteLine();
+            Console.WriteLine(users);
 
             if(!users.Any())
             {
-                var errors = new List<string> { "There is no users" };
+                var errors = new List<string> { "There are no users" };
                 return BadRequest(new UserFailedResponse { Errors = errors });
             }
 
             return Ok(new UserSuccessResponse { Result = users, Message = "Request proceed successfully" });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Details(string id)
+        {
+            var user = _userRepository.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
         }
     }
 }
