93b08642e98078843d61ffd4722820ce193c4954
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-10-23 22:06:25 +0200

    added update and delete to motivate texts

---
+++ Shelter.API.Tests/Shelter.API.Tests/UserRepository.Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Shelter.API.Data.Repositories;
+
+namespace Shelter.API.Tests
+{
+    [TestFixture]
+    class UserRepositoryTest
+    {
+        [Test]
+        public void Get_AllMails_List();
+        {
+            UserRepositoryTest repo = new UserRepositoryTest(); 
+        }
+    }
+}
